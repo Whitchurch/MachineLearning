9223b0d3eaea996b05e4f7207c2229a08a7b2ffb
Whitchurch <40561788+Whitchurch@users.noreply.github.com>
2020-08-16 16:46:56 -0700

    Octave code for linear regression

---
+++ machine-learning-ex1/ex1/ex_multi.m
@@ -0,0 +1,28 @@
+## Copyright (C) 2018 whitchurch85
+## 
+## This program is free software: you can redistribute it and/or modify it
+## under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+## 
+## This program is distributed in the hope that it will be useful, but
+## WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see
+## <https://www.gnu.org/licenses/>.
+
+## -*- texinfo -*- 
+## @deftypefn {} {@var{retval} =} ex_multi (@var{input1}, @var{input2})
+##
+## @seealso{}
+## @end deftypefn
+
+## Author: whitchurch85 <whitchurch85@DESKTOP-2CU1CAH>
+## Created: 2018-11-13
+
+function retval = ex_multi (input1, input2)
+
+endfunction
